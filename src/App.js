71941c167d8f3c5eeb9eec3824e32e6bbdf37592
Nguyen Huy <saviorlight271@gmail.com>
2022-06-07 18:05:51 +0700

    second-update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Học ReactJS tại trang chủ
         </a>
       </header>
     </div>
